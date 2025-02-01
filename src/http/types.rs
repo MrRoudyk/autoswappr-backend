acdd0cb843543d7f3fc00fdeceb33c80cf1c4ebf
Anton Rudenko <mrroudyk09@gmail.com>
2025-02-01 12:27:58 +0400

    refactor: move unit tests to integration folder and add addresses validation in auto_swap_service

--- src/http/types.rs
+++ src/http/types.rs
@@ -138,35 +138,3 @@ pub fn is_valid_address(address: &str) -> bool {
         && address.len() == ADDRESS_LENGTH
         && address[2..].chars().all(|c| c.is_ascii_hexdigit())
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_valid_address() {
-        assert!(is_valid_address(
-            "0x40ca979f20ed76f960dc719457eaf0cef3b2c3932d58435b9192a58bc56c1e40"
-        ));
-    }
-
-    #[test]
-    fn test_invalid_addresses() {
-        let long_zeros = "0".repeat(64);
-        let long_as = "a".repeat(67);
-
-        let invalid_addresses = vec![
-            "",
-            "0x",
-            "0x123",
-            "123456",
-            "0xXYZabc",
-            &long_zeros,
-            &long_as,
-        ];
-
-        for addr in invalid_addresses {
-            assert!(!is_valid_address(addr));
-        }
-    }
-}
